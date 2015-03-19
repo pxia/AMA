466e1da14b0b8e9edee91844896c80b23fccd694
xiaozhuyfk <xiaozhuyfk@gmail.com>
2015-03-19 04:17:39 -0400

    ask.py

---
+++ src/AMA/ask.py
@@ -0,0 +1,61 @@
+import nltk
+
+beVerb = set(["is", "are", "was", "were", "am"])
+#verbTag = set(["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"])
+verbTag = {"VB":"Do", "VBD":"Did", "VBG":"Do", "VBN":"Did", "VBP":"Do", "VBZ":"Does"}
+
+def yesNoQuestion(text, tags):
+    verbIndex = None
+    beQuestion = True
+    questionVerb = ""
+    result = []
+    
+    for i in xrange(len(tags)):
+        (word, tag) = tags[i]
+        if (tag in verbTag):
+            verbIndex = i
+            if (word in beVerb):
+                questionVerb = word[0].upper() + word[1:]
+            else:
+                beQuestion = False
+                questionVerb = verbTag[tag]
+            break
+
+    if (verbIndex != None):
+        result.append(questionVerb)
+        for i in xrange(verbIndex):
+            result.append(text[i])
+            
+        if (not beQuestion):
+            result.append(text[verbIndex])
+            
+        for i in range(verbIndex+1, len(text)):
+            if ((i < len(text)-1) or (i == len(text)-1 and text[i].isalpha())):
+                result.append(text[i])
+        result.append("?")
+
+    return [" ".join(result)]
+            
+
+def whQuestion(text, tags):
+    return []
+
+def choiceQuestion(text, tags):
+    return []
+
+
+def questionGenerator(sentence):
+    result = []
+    
+    text = nltk.word_tokenize(sentence)
+    tags = nltk.pos_tag(text)
+
+    result += yesNoQuestion(text, tags)
+    result += whQuestion(text, tags)
+    result += choiceQuestion(text, tags)
+
+    print result
+
+
+questionGenerator("Prime Minister Vladimir V. Putin is the country's paramount leader.")
+questionGenerator("Prime Minister Vladimir V. Putin returned to Moscow to oversee the federal response.")
