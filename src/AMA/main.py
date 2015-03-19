337a240a79b6fe266ba0d4dbacdda101549de42f
Peter Xia <aattyy@gmail.com>
2015-03-19 11:01:05 -0400

    extract kw from question and find relevent sentences in article

--- src/AMA/main.py
+++ src/AMA/main.py
@@ -1,23 +1,30 @@
-import re
+import sys, util;
 
 def ask():
     print "ask"
+    articleFile = sys.argv[1]
+    nquestions  = int(sys.argv[2])
 
 def answer():
     print "answer"
+    articleFile  = sys.argv[1]
+    questionFile = sys.argv[2]
+
+    article      = readFile(article)
+    questions    = util.readFile(questionFile).splitlines()
 
 
 
-# assume document is accessible within the class, 
+# assume document is accessible within the class,
 # and is already parsed into a raw string
 def locateKeywords(keywords):
-	D = {}
-	for kw in keywords:
-		D[kw] = map(lambda m: m.start(), re.finditer(kw, document))
+    D = {}
+    for kw in keywords:
+        D[kw] = map(lambda m: m.start(), re.finditer(kw, document))
 
-	# find the area that the keywords are densest in
+    # find the area that the keywords are densest in
 
 
-	
+
 
 
