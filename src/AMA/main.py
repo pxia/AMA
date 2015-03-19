d5aaa6f42040b15021b6bacf0fb13e635ecab954
bethovenia <donghui.luan@gmail.com>
2015-03-18 23:12:56 -0400

    locateKeyword DP algo

--- src/AMA/main.py
+++ src/AMA/main.py
@@ -1,5 +1,23 @@
+import re
+
 def ask():
     print "ask"
 
 def answer():
     print "answer"
+
+
+
+# assume document is accessible within the class, 
+# and is already parsed into a raw string
+def locateKeywords(keywords):
+	D = {}
+	for kw in keywords:
+		D[kw] = map(lambda m: m.start(), re.finditer(kw, document))
+
+	# find the area that the keywords are densest in
+
+
+	
+
+
