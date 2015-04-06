76a0de28ce3f9bcfc0fd2dc0a255bcec769fc2a6
xiaozhuyfk <xiaozhuyfk@gmail.com>
2015-04-05 20:19:44 -0400

    dependencies

--- src/AMA/dependencies.py
+++ src/AMA/dependencies.py
@@ -4,12 +4,12 @@ import re
 import ner
 
 from simplejson import loads
-from corenlp import StanfordCoreNLP
+from lib.corenlp import StanfordCoreNLP
 
 
 
 print "Loading CoreNLP ... "
-corenlp_dir = "stanford-corenlp-full-2014-08-27/"
+corenlp_dir = "lib/stanford-corenlp-full-2014-08-27/"
 corenlp = StanfordCoreNLP(corenlp_dir)  # wait a few minutes...
 
 
@@ -33,21 +33,23 @@ def preprocess(sentences):
     dependency_table = {}
     for sent in sentences:
         if sent == "": continue
-
         result = loads(corenlp.parse(sent))['sentences'][0]
         dependencies = result['dependencies']
+        parsetree = result['parsetree']
         for dependency in dependencies:
             relation = dependency[0]
-            if relation not in dependency_table:
-                dependency_table[relation] = [(dependency[1], dependency[2])]
-            else:
-                dependency_table[relation].append((dependency[1], dependency[2]))
-        #print dependencies
+            #if relation not in dependency_table:
+            #    dependency_table[relation] = [(dependency[1], dependency[2])]
+            #else:
+            #    dependency_table[relation].append((dependency[1], dependency[2]))
+        print dependencies
+        print parsetree
+        print
     return dependency_table
 
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-data = readFile("0.txt").decode('utf8')
+data = readFile("text/0.txt").decode('utf8')
 sentences = tokenizer.tokenize(data)
 table = preprocess(sentences)
 print table
